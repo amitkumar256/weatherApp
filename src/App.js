c1e67c521d92f144ebc6c923ae0e40b643c8411a
amit <amit256375@gmail.com>
2023-01-03 19:42:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Temp from './components/weather/temp';
+
+
+
+function App() {
+  return (
+    <div className="">
+      <Temp/>
+     
+    </div>
+  );
+}
+
+export default App;
